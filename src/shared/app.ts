2c9b9d8554afeadfb57dffc054117985b9cc87b2
Guilherme <guilherme.flesch92@gmail.com>
2022-07-26 12:36:42 -0300

    capturando erros pelo servidor

--- src/shared/app.ts
+++ src/shared/app.ts
@@ -1,11 +1,27 @@
 import cors from "cors";
-import "dotenv/config"
-import express from "express";
+import "dotenv/config";
+import "express-async-errors";
+import express, { NextFunction } from "express";
+import { AppError } from "./errors";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 app.use(cors());
+app.use(router);
+
+app.use((err: Error, _request: Request, response: Response, _next: NextFunction) => {
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            message: err.message
+        });
+    }
+    return response.status(500).json({
+        status: "error",
+        message: `Internal Server Error - ${err.message}` 
+    });
+});
 
 export { app }
